c63544ccdcfb9b8d7c86c3d3f9903780a2ff8b6e
Supriyo Chatterjee <supriyo@aidias.onmicrosoft.com>
2023-11-11 22:19:09 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import os
+import dash_bootstrap_components as dbc
+
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@callback(Output('display-value', 'children'), Input('dropdown', 'value'))
+def display_value(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
